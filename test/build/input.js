9a666ab3b88fba545e2e9975d61dbf72efa293aa
Paul Miller <paul@paulmillr.com>
2025-08-20 17:34:15 +0000

    Fix ci single-file build

---
+++ test/build/input.js
@@ -0,0 +1,6 @@
+export * as dh from '../../dh.js';
+export * as dsa from '../../dsa.js';
+export * as elgamal from '../../elgamal.js';
+export * as primality from '../../primality.js';
+export * as rsa from '../../rsa.js';
+export * as utils from '../../utils.js';
